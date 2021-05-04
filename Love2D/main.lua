80be0bd604be0d4a40f10d487f46b58fa00fb963
eVoDotEXE <83671753+eVoDotEXE@users.noreply.github.com>
2021-05-04 22:47:17 +0200

    Template

--- Love2D/main.lua
+++ Love2D/main.lua
@@ -10,6 +10,9 @@ math.randomseed(love.timer.getTime())
 -- Returns the distance between two points.
 function math.dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
 
+--[[
+
+]]
 
 function love.load()
   
